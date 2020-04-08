ab0c0ed7453f9de6584687b835b1efbfab4da2f9
hou_fx <hou_fx@9hjf.com>
2020-04-08 17:12:29 +0800

    init

--- main.go
+++ main.go
@@ -10,10 +10,8 @@ import (
 	"go-blog/src/common"
 	"go-blog/src/controller"
 	"go-blog/src/model"
-	"log"
 	"os"
 	"path/filepath"
-	"strings"
 )
 
 func main() {
@@ -24,12 +22,12 @@ func main() {
 	defer db.Close()
 
 	router := gin.Default()
-	//router.LoadHTMLGlob(GetCurrentPath()+"/src/go-blog/templates/**/*")
-	//router.Static("/static", GetCurrentPath()+"/src/go-blog/static")
+
 	router.LoadHTMLGlob(filepath.Join(os.Getenv("GOPATH"), "/src/go-blog/templates/**/*"))
 	router.Static("/static", filepath.Join(os.Getenv("GOPATH"), "/src/go-blog/static"))
 
 	gob.Register(model.UserInfo{})
+
 	store := cookie.NewStore([]byte(common.ContextUserKey))
 	store.Options(sessions.Options{HttpOnly: true, MaxAge: 3600 * 24, Path: "/"})
 	router.Use(sessions.Sessions(common.Session, store))
@@ -46,18 +44,4 @@ func main() {
 	router.GET("/blog/new", controller.NewBlog)
 
 	router.Run(":8000")
-}
-func getCurrentDirectory() string {
-	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
-	if err != nil {
-		fmt.Print(err)
-	}
-	return strings.Replace(dir, "\\", "/", -1)
-}
-func GetCurrentPath() string {
-	dir, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err)
-	}
-	return strings.Replace(dir, "\\", "/", -1)
 }
