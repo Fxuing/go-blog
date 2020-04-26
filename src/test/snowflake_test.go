9d091ecdb5754ee1e11b9c44b8f79fb6b477d0af
hou_fx <hou_fx@9hjf.com>
2020-04-26 14:03:38 +0800

    111

--- src/test/snowflake_test.go
+++ src/test/snowflake_test.go
@@ -12,16 +12,16 @@ var (
 	sequence_bit    int64 = 12                                      // 序列号占用位数
 	machine_int     int64 = 10                                      // 机器标识所占位数
 	timestamp_left  int64 = sequence_bit + machine_int              // 时间戳位移位数
-	max_sequence    int64 = -1 << sequence_bit                      // 最大序列号
-	max_machine_id  int64 = -1 << machine_int                       // 最大机器编号
+	max_sequence    int64 = -1 ^ (-1 << sequence_bit)               // 最大序列号
+	max_machine_id  int64 = -1 ^ (-1 << machine_int)                // 最大机器编号
 	machineIdPart   int64 = (9123 & max_machine_id) << sequence_bit // 生成id 机器标识部分
 	sequence        int64 = 0                                       // 序列号
 	last_stamp      int64 = -1
 )
 
 func nextId(lock *sync.RWMutex) (int64, error) {
-	lock.Lock()
-	defer lock.Unlock()
+	//lock.Lock()
+	//defer lock.Unlock()
 	currentStamp := time.Now().UnixNano() / 1e6 // 当前是时间戳（毫秒）
 	// 当前时间小于最后生成的时间
 	if currentStamp < last_stamp {
